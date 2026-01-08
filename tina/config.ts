6ceb9e271369c6075d4f442fd21454e26a0cdca1
adnandossaji <nan.dossaji@gmail.com>
2026-01-07 19:50:47 -0600

    Add tinacms

---
+++ tina/config.ts
@@ -0,0 +1,76 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+    process.env.GITHUB_BRANCH ||
+    process.env.VERCEL_GIT_COMMIT_REF ||
+    process.env.HEAD ||
+    "main";
+
+export default defineConfig({
+    branch,
+
+    // Get this from tina.io
+    clientId: process.env.TINA_CMS_CLIENT_ID,
+    // Get this from tina.io
+    token: process.env.TINA_CMS_CONTENT_TOKEN,
+
+    build: {
+        outputFolder: "admin",
+        publicFolder: "public",
+    },
+    media: {
+        tina: {
+            mediaRoot: "",
+            publicFolder: "public",
+        },
+    },
+    // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/r/content-modelling-collections/
+    schema: {
+        collections: [
+            {
+                name: "blog",
+                label: "Blog Posts",
+                path: "src/content/blog",
+                format: "md",
+                fields: [
+                    {
+                        type: "string",
+                        name: "title",
+                        label: "Title",
+                        isTitle: true,
+                        required: true,
+                    },
+                    {
+                        type: "string",
+                        name: "description",
+                        label: "Description",
+                        required: true,
+                    },
+                    {
+                        type: "datetime",
+                        name: "pubDate",
+                        label: "Publication Date",
+                        required: true,
+                    },
+                    {
+                        type: "datetime",
+                        name: "updatedDate",
+                        label: "Updated Date",
+                    },
+                    {
+                        type: "image",
+                        name: "heroImage",
+                        label: "Hero Image",
+                    },
+                    {
+                        type: "rich-text",
+                        name: "body",
+                        label: "Body",
+                        isBody: true,
+                    },
+                ],
+            },
+        ],
+    },
+});
